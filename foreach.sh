b4c28cb76135d1afea46c99655a7763fbd11bcc9
Matt Feldman <mattfel@stanford.edu>
2017-03-29 15:01:42 -0700

    change script

--- foreach.sh
+++ foreach.sh
@@ -5,4 +5,9 @@ for branch in ${branches[@]}; do
   git checkout -b $branch
   git pull origin $branch
   sed -i 's/on_failure: change/on_failure: never/g' .travis.yml
+  git checkout --theirs -- results
+  git checkout --theirs -- README.md
+  git add -A
+  git commit -m "foreach script"
+  git push
 done
